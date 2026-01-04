a8819dd902f9a4d55c1de10194144bbc12af2788
Grant Schwidder <gschwid@gmail.com>
2026-01-04 04:25:52 +0000

    Working on creating occupancy filling for rotated squares

--- src/map_parser.cpp
+++ src/map_parser.cpp
@@ -91,85 +91,106 @@ void MapParser::getObstacles(const gz::sim::EntityComponentManager &_ecem)
 
     _ecem.Each<gz::sim::components::Collision, gz::sim::components::Geometry, gz::sim::components::Name>(
         [this, &_ecem](const gz::sim::Entity &_entity, const gz::sim::components::Collision *_model, const gz::sim::components::Geometry *_geom, const gz::sim::components::Name *name) -> bool
-        {   
-
+        {
             // Dont want to include ground plane
-            if (name->Data() != "ground_plane") {
+            if (name->Data() != "ground_plane")
+            {
 
-            // Get world pose of each collision object
-            gz::math::Pose3d worldPose = gz::sim::worldPose(_entity, _ecem);
-            gzmsg << "X: " << worldPose.Pos().X() << " Y: " << worldPose.Pos().Y() << " Z: " << worldPose.Pos().Z() << std::endl;
+                // Get world pose of each collision object
+                gz::math::Pose3d worldPose = gz::sim::worldPose(_entity, _ecem);
+                gzmsg << "X: " << worldPose.Pos().X() << " Y: " << worldPose.Pos().Y() << " Z: " << worldPose.Pos().Z() << std::endl;
 
-            // Get the size depending on what type of object we are looking at
-            gz::math::Vector3d scale;
+                // Get the size depending on what type of object we are looking at
+                gz::math::Vector3d scale;
 
-            if (_geom->Data().Type() == sdf::GeometryType::BOX)
-            {   
+                if (_geom->Data().Type() == sdf::GeometryType::BOX)
+                {
 
-                // Making a draft of the occupancy grid filling feature.
-                scale = _geom->Data().BoxShape()->Size();
-                gzmsg << "This is a box" << std::endl;
-                int x_inarray = int ((worldPose.X() - (this->origin.position.x)) / GRID_SIZE);
-                int y_inarray = int ((worldPose.Y() - (this->origin.position.y)) / GRID_SIZE);
-                double half_x = (scale.X() / 2.0) / GRID_SIZE;
-                double half_y = (scale.Y() / 2.0) / GRID_SIZE;
+                    // Making a draft of the occupancy grid filling feature.
+                    scale = _geom->Data().BoxShape()->Size();
+                    gzmsg << "This is a box" << std::endl;
 
-                int x_start = std::floor(x_inarray - half_x);
-                int x_end = std::ceil(x_inarray + half_x);
-                int y_start = std::floor(y_inarray - half_y);
-                int y_end = std::ceil(y_inarray + half_y);
+                    int x_inarray = int((worldPose.X() - (this->origin.position.x)) / GRID_SIZE);
+                    int y_inarray = int((worldPose.Y() - (this->origin.position.y)) / GRID_SIZE);
+                    double half_x = (scale.X() / 2.0) / GRID_SIZE;
+                    double half_y = (scale.Y() / 2.0) / GRID_SIZE;
 
-                std::cout << x_start << " " << x_end << " " << y_start << " " << y_end << std::endl;
+                    // Get local corners of box
+                    gz::math::v7::Vector3d test{1,1,1};
+                    gz::math::v7::Quaternion<double> rotation = worldPose.Rot();
+                    gz::math::v7::Vector3d tl{-(scale.X() / 2.0), scale.Y() / 2.0, 0};
+                    gz::math::v7::Vector3d bl{-(scale.X() / 2.0), -(scale.Y() / 2.0), 0};
+                    gz::math::v7::Vector3d tr{scale.X() / 2.0, scale.Y() / 2.0, 0};
+                    gz::math::v7::Vector3d br{scale.X() / 2.0, -(scale.Y() / 2.0), 0};
+                    
+                    // Rotate local coordinates into world coordinates
+                    gz::math::v7::Vector3d wtl = (rotation * tl) + worldPose.Pos();
+                    gz::math::v7::Vector3d wbl = (rotation * bl) + worldPose.Pos();
+                    gz::math::v7::Vector3d wtr = (rotation * tr) + worldPose.Pos();
+                    gz::math::v7::Vector3d wbr = (rotation * br) + worldPose.Pos();
+                    
+                    std::cout << "----------------\n" << wtl << " " << wbl << " " << wtr << " " << wbr << std::endl;
 
-                for (int i = x_start; i < x_end; i++){
-                    for (int j = y_start; j < y_end; j++){
-                        occupancy_grid[i + grid_width * j] = OCCUPIED;
+                    int x_start = std::floor(x_inarray - half_x);
+                    int x_end = std::ceil(x_inarray + half_x);
+                    int y_start = std::floor(y_inarray - half_y);
+                    int y_end = std::ceil(y_inarray + half_y);
+
+                    std::cout << x_start << " " << x_end << " " << y_start << " " << y_end << std::endl;
+
+                    for (int i = x_start; i < x_end; i++)
+                    {
+                        for (int j = y_start; j < y_end; j++)
+                        {
+                            occupancy_grid[i + grid_width * j] = OCCUPIED;
+                        }
                     }
+                    gzmsg << origin.position.x << " " << origin.position.y << std::endl;
+                    gzmsg << "Name " << name->Data() << " Scale X: " << scale.X() << " Scale Y: " << scale.Y() << " Scale Z: " << scale.Z() << std::endl;
+                    gzmsg << "x_inarray " << x_inarray << " y_inarray " << y_inarray << std::endl;
                 }
-                gzmsg << "x_inarray " << x_inarray << " y_inarray " << y_inarray << std::endl;
 
+                else if (_geom->Data().Type() == sdf::GeometryType::CYLINDER)
+                {
+                    scale.X() = _geom->Data().CylinderShape()->Radius() * 2;
+                    scale.Y() = scale.X();
+                    scale.Z() = _geom->Data().CylinderShape()->Length();
+                    gzmsg << "This is a cylinder" << std::endl;
+                }
+
+                else if (_geom->Data().Type() == sdf::GeometryType::SPHERE)
+                {
+                    scale.X() = _geom->Data().SphereShape()->Radius() * 2;
+                    scale.Y() = scale.X();
+                    scale.Z() = scale.X();
+                    gzmsg << "This is a sphere" << std::endl;
+                }
+
+                else if (_geom->Data().Type() == sdf::GeometryType::PLANE)
+                {
+                    scale.X() = _geom->Data().PlaneShape()->Size().X();
+                    scale.Y() = _geom->Data().PlaneShape()->Size().Y();
+                    scale.Z() = 0;
+                    gzmsg << "This is a plane" << std::endl;
+                }
+
+                else if (_geom->Data().Type() == sdf::GeometryType::MESH)
+                {
+
+                    auto path = gz::sim::asFullPath(_geom->Data().MeshShape()->Uri(), _geom->Data().MeshShape()->FilePath());
+                    gzmsg << _geom->Data().MeshShape()->FilePath() << " " << _geom->Data().MeshShape()->Uri() << std::endl;
+                }
+
+                else
+                {
+                    gzmsg << "This is unrecognized BRU" << std::endl;
+                    return true;
+                }
+
+                gzmsg << "Name " << name->Data() << " Scale X: " << scale.X() << " Scale Y: " << scale.Y() << " Scale Z: " << scale.Z() << std::endl;
             }
-
-            else if (_geom->Data().Type() == sdf::GeometryType::CYLINDER)
-            {
-                scale.X() = _geom->Data().CylinderShape()->Radius() * 2;
-                scale.Y() = scale.X();
-                scale.Z() = _geom->Data().CylinderShape()->Length();
-                gzmsg << "This is a cylinder" << std::endl;
-            }
-
-            else if (_geom->Data().Type() == sdf::GeometryType::SPHERE)
-            {
-                scale.X() = _geom->Data().SphereShape()->Radius() * 2;
-                scale.Y() = scale.X();
-                scale.Z() = scale.X();
-                gzmsg << "This is a sphere" << std::endl;
-            }
-
-            else if (_geom->Data().Type() == sdf::GeometryType::PLANE)
-            {
-                scale.X() = _geom->Data().PlaneShape()->Size().X();
-                scale.Y() = _geom->Data().PlaneShape()->Size().Y();
-                scale.Z() = 0;
-                gzmsg << "This is a plane" << std::endl;
-            }
-
-            else if (_geom->Data().Type() == sdf::GeometryType::MESH) {
-
-                auto path = gz::sim::asFullPath(_geom->Data().MeshShape()->Uri(), _geom->Data().MeshShape()->FilePath());
-                gzmsg << _geom->Data().MeshShape()->FilePath() << " " << _geom->Data().MeshShape()->Uri() << std::endl;
-                
-            }
-
-            else {
-                gzmsg << "This is unrecognized BRU" << std::endl;
-                return true;
-            }
-
-            gzmsg << "Name " << name->Data() << " Scale X: " << scale.X() << " Scale Y: " << scale.Y() << " Scale Z: " << scale.Z() << std::endl;
-    }
-    return true;
-});
+            return true;
+        });
 }
 
 void MapParser::getDimensions(gz::sim::EntityComponentManager &_ecem)
